9e90208e50274e9c9d04f031e9c73131ea0b436d
Sangho Kim <shkimkitecs@gmail.com>
2022-08-04 15:26:36 +0900

    Feat: 보이스 피싱 데이터 크롤링 기능 추가 구현

--- fds/crawler/get_data.py
+++ fds/crawler/get_data.py
@@ -23,6 +23,19 @@ def get_links(siteUrl, pageUrl):
     return pageList
 
 
+def get_mp3_links(siteUrl, pageUrl):
+    pageList = []
+    html = urlopen(siteUrl+pageUrl)
+    bsObject = BeautifulSoup(html, 'lxml', from_encoding='utf-8')
+
+    crawlHref = bsObject.select('div.bd-list > table td.title > a')
+
+    for tag in crawlHref:
+        pageList.append(siteUrl+tag['href'])
+
+    return pageList
+
+
 def get_media_links(pageList, siteUrl):
     media_list = []
     for pageUrl in pageList:
@@ -35,6 +48,25 @@ def get_media_links(pageList, siteUrl):
     return media_list
 
 
+def get_mp3(pageList, siteUrl, total):
+    media_list = []
+    idx = 1
+    for pageUrl in pageList:
+        html = urlopen(pageUrl)
+        bsObject = BeautifulSoup(html, 'lxml', from_encoding='utf-8')
+        mediaUrl = (bsObject.select('div.dbdata > video'))[0]['src']
+        mediaName = str(total+idx)+'.mp3'
+        idx += 1
+
+        media_list.append([mediaName, siteUrl+mediaUrl])
+    return media_list
+
+
 def download_media(media):
     urlretrieve(media[1], PARENT_DIR+'/data/this_voice/'+media[0])
+    print(media[0], ' download completed')
+
+
+def download_mp3(media):
+    urlretrieve(media[1], PARENT_DIR+'/data/fraud/'+media[0])
     print(media[0], ' download completed')
--- fds/crawler/imposter_crawler.py
+++ fds/crawler/imposter_crawler.py
@@ -1,8 +1,9 @@
 from multiprocessing import Pool
 from os import path as Path
 from os import remove
+from turtle import down
 from utility.make_dir import makedirs
-from get_data import get_links, get_media_links, download_media
+from get_data import download_mp3, get_links, get_media_links, download_media, get_mp3, get_mp3_links
 
 
 # 현재 디렉토리 주소 /fds/crawler
@@ -14,37 +15,38 @@ PARENT_DIR = Path.dirname((CUR_DIR))
 if __name__ == '__main__':
 
     siteUrl = 'https://www.fss.or.kr'
-    pageUrl = ['/fss/bbs/B0000203/list.do?menuNo=200686&bbsId=&cl1Cd=&pageIndex=',
+    pageUrl = ['/fss/bbs/B0000207/list.do?menuNo=200691&bbsId=&cl1Cd=&pageIndex=',
                '&sdate=&edate=&searchCnd=1&searchWrd=']
     makedirs(PARENT_DIR+'/data')
-    makedirs(PARENT_DIR+'/data/this_voice')
-
-    pageIndex = 1
+    makedirs(PARENT_DIR+'/data/imposter')
+    pageIndex = 20
+    total = 375
     pageList = []
     mediaList = []
+
     while True:
         try:
-            pageList = get_links(siteUrl, pageUrl[0]+str(pageIndex)+pageUrl[1])
-            mediaList = get_media_links(pageList, siteUrl)
+            pageList = get_mp3_links(siteUrl, pageUrl[0]+str(pageIndex)+pageUrl[1])
+            mediaList = get_mp3(pageList, siteUrl, total)
 
             if len(pageList) < 1:
                 break
 
-            print('{}page : {} media files'.format(pageIndex, len(mediaList)))
+            print('{}page: {} media files'.format(pageIndex, len(mediaList)))
 
-            pool = Pool(processes=min(4, len(mediaList)))
-            pool.map(download_media, mediaList)
+            pool = Pool(processes=min(5, len(mediaList)))
+            pool.map(download_mp3, mediaList)
             pool.close()
             pool.join()
 
             pageIndex += 1
+            total += (len(mediaList))
             pageList.clear()
             mediaList.clear()
-
         except Exception:
             print('Exception in {} page, Re-try crawling this page...'.format(pageIndex))
 
             for f in mediaList:
-                file_path = PARENT_DIR+'/data/this_voice/'+f[0]
+                file_path = PARENT_DIR+'/data/fraud/'+f[0]
                 if Path.exists(file_path):
                     remove(file_path)
