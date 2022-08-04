c72ab574784085df1660b90c5e273e8765427bf7
Sangho Kim <shkimkitecs@gmail.com>
2022-08-04 15:28:39 +0900

    Feat: mp3 파일 flac 변환 기능 구현

---
+++ fds/converter/mp3toflac.py
@@ -0,0 +1,17 @@
+from pydub import AudioSegment
+import os
+
+# 현재 디렉토리 주소 /fds/crawler
+CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+# 부모 디렉토리 주소 /fds/
+PARENT_DIR = os.path.dirname((CUR_DIR))
+
+file_list = os.listdir('{}/data/fraud'.format(PARENT_DIR))
+print('{} files will be converted...'.format(len(file_list)))
+for i in range(len(file_list)):
+    file_name = file_list[i]
+    print('{} / {} files covert completed...'.format(i+1, len(file_list)), end='\r')
+    sound = AudioSegment.from_file("{}/data/fraud/{}".format(PARENT_DIR, file_name))
+    sound.export("{}/data/fraud_convert/{}.flac".format(PARENT_DIR, file_name[:-4]), format='flac')
+print('')
+print('done')
