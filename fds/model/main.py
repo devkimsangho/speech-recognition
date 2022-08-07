1b6f7d0c09ce9203ea66abd3458e47b1beb0a858
Sangho Kim <esaitchkim@gmail.com>
2022-08-07 19:17:41 +0900

    Add: 모델 재 학습 및 Preprocess 재정의

--- fds/model/main.py
+++ fds/model/main.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     # dataset = pd.read_csv('{}/data/dataset/dataset.csv'.format(PARENT_DIR), names=['text', 'fishing'])
     # dataset = preprocessing.remove_spectial_character(dataset)
     # dataset['length'] = dataset['text'].apply(lambda x: len(x))
-    # # dataset = dataset.drop(dataset[dataset['length'] > 6000].index)
+    # dataset = dataset.drop(dataset[dataset['length'] > 6000].index)
 
     # print('Dataset 분리\n')
     # # 데이터 셋 분리
@@ -43,22 +43,22 @@ if __name__ == '__main__':
     # test = dataset[4784:]
     # train = train.drop(train[train['length'] > 6000].index)
 
-    # # print('토큰화 \n')
+    # print('토큰화 \n')
     # # # 토큰화 진행
     # train['tokenized'] = train['text(clean)'].apply(preprocessing.tokenizer)
     # test['tokenized'] = test['text(clean)'].apply(preprocessing.tokenizer)
 
-    # # print('train/val 분리\n')
+    # print('train/val 분리\n')
     # # train set test-val 분리
     # train, val = train_test_split(train, test_size=0.3, random_state=42)
     train = pd.read_csv('{}/data/dataset/train_6000.csv'.format(PARENT_DIR), names=['text', 'fishing', 'length', 'text(clean)', 'tokenized'])
     val = pd.read_csv('{}/data/dataset/val_6000.csv'.format(PARENT_DIR), names=['text', 'fishing', 'length', 'text(clean)', 'tokenized'])
     test = pd.read_csv('{}/data/dataset/test_6000.csv'.format(PARENT_DIR), names=['text', 'fishing', 'length', 'text(clean)', 'tokenized'])
 
-    print(len(train)+len(val)+len(test))
-    print('w2v model load\n')
-    bigrams = Phrases(sentences=train.tokenized)
-    trigrams = Phrases(sentences=bigrams[train.tokenized])
+    # print(len(train)+len(val)+len(test))
+    # print('w2v model load\n')
+    # bigrams = Phrases(sentences=train.tokenized)
+    # trigrams = Phrases(sentences=bigrams[train.tokenized])
 
     # tr = pd.DataFrame(train)
     # tr.to_csv('{}/data/dataset/train_6000.csv'.format(PARENT_DIR), header=None, index=None)
@@ -69,51 +69,34 @@ if __name__ == '__main__':
     N_SPLITS = 5
     EMBEDDING_SIZE = 256
 
-    tri_model = Word2Vec(sentences=trigrams[bigrams[train.tokenized]], vector_size=EMBEDDING_SIZE, window=10, min_count=10, workers=4, sg=1)
-    tri_model.wv.save_word2vec_format('w2v_model')
-    # tri_model = KeyedVectors.load_word2vec_format('{}/w2v_model'.format(ROOT_DIR))
+    # tri_model = Word2Vec(sentences=trigrams[bigrams[train.tokenized]], vector_size=EMBEDDING_SIZE, window=10, min_count=10, workers=4, sg=1)
+    # tri_model.wv.save_word2vec_format('w2v_model')
+    tri_model = KeyedVectors.load_word2vec_format('{}/w2v_model'.format(ROOT_DIR))
     print('x data tokenized\n')
     X_data = train.tokenized
     input_length = max(list(map(len, X_data)))
 
     print('X, pad_sequences\n')
     X = pad_sequences(
-        sequences=vectorize_data(X_data, vocab=tri_model.wv.key_to_index),
+        sequences=vectorize_data(X_data, vocab=tri_model.key_to_index),
         maxlen=input_length,
         padding='post'
     )
     y = train['fishing'].values
 
     print('X_test, X_val pad_sequences\n')
-    X_test = pad_sequences(sequences=vectorize_data(test.tokenized, vocab=tri_model.wv.key_to_index), maxlen=input_length, padding='post')
-    X_val = pad_sequences(sequences=vectorize_data(val.tokenized, vocab=tri_model.wv.key_to_index), maxlen=input_length, padding='post')
+    X_test = pad_sequences(sequences=vectorize_data(test.tokenized, vocab=tri_model.key_to_index), maxlen=input_length, padding='post')
+    X_val = pad_sequences(sequences=vectorize_data(val.tokenized, vocab=tri_model.key_to_index), maxlen=input_length, padding='post')
     y_val = val['fishing'].values
 
     print('build_model\n')
-    model_lstm = build_model(embedding_matrix=tri_model.wv.vectors, input_length=X.shape[1])
+    model_lstm = build_model(embedding_matrix=tri_model.vectors, input_length=X.shape[1])
     early_stopping_callback = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto')
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint('{}/lstm_model.h5'.format(CUR_DIR), verbose=0, save_best_only=True, monitor='val_loss')
 
     print('trained\n')
     batch_size = 32
-    # trained = model_lstm.fit(X, y, epochs=30, batch_size=batch_size, validation_data=(X_val, y_val), callbacks=[early_stopping_callback, model_checkpoint_callback])
-
-    # import matplotlib.pyplot as plt
-    # plt.plot(trained.history['acc'])
-    # plt.plot(trained.history['val_acc'])
-    # plt.title('Model accuracy')
-    # plt.ylabel('Accuracy')
-    # plt.xlabel('Epoch')
-    # plt.legend(['Train', 'Val'], loc='upper left')
-    # plt.show()
-
-    # plt.plot(trained.history['loss'])
-    # plt.plot(trained.history['val_loss'])
-    # plt.title('Model loss')
-    # plt.ylabel('Loss')
-    # plt.xlabel('Epoch')
-    # plt.legend(['Train', 'Val'], loc='upper left')
-    # plt.show()
+    trained = model_lstm.fit(X, y, epochs=30, batch_size=batch_size, validation_data=(X_val, y_val), callbacks=[early_stopping_callback, model_checkpoint_callback])
 
     trained = model_lstm.load_weights('{}/lstm_model.h5'.format(CUR_DIR))
     model_lstm.evaluate(X_val, y_val, batch_size=batch_size, workers=4)
