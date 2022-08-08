6db21ee4341f860989676eaba7566a30eaf49168
Sangho Kim <shkimkitecs@gmail.com>
2022-08-08 12:32:13 +0900

    Remove: 불필요한 파일 제거

--- fds/model/vectorize_data_f.py
+++
@@ -1,8 +0,0 @@
-def vectorize_data(data, vocab: dict) -> list:
-    print('Vectorize sentences...', end='\r')
-    keys = list(vocab.keys())
-    def filter_unknown(word): return vocab.get(word, None) is not None
-    def encode(review): return list(map(keys.index, filter(filter_unknown, review)))
-    vectorized = list(map(encode, data))
-    print('Vectorie sentence.. (done)')
-    return vectorized
