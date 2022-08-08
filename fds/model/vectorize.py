bb9baac8f92f7ab507b2b4dc59b4624b18360ede
Sangho Kim <shkimkitecs@gmail.com>
2022-08-08 12:30:20 +0900

    Feat: 데이터 패딩 및 정수 인덱싱  기능 구현

---
+++ fds/model/vectorize.py
@@ -0,0 +1,21 @@
+from keras_preprocessing.sequence import pad_sequences
+
+
+def vectorize_data(data, vocab: dict) -> list:
+    print('Vectorize sentences...', end='\r')
+    keys = list(vocab.keys())
+    def filter_unknown(word): return vocab.get(word, None) is not None
+    def encode(review): return list(map(keys.index, filter(filter_unknown, review)))
+    vectorized = list(map(encode, data))
+    print('Vectorie sentence.. (done)')
+    return vectorized
+
+
+def integer_indexing(data, maxlen, tri_model, padding='post'):
+    x = pad_sequences(
+        sequences=vectorize_data(data, vocab=tri_model.vocab),
+        maxlen=maxlen,
+        padding=padding
+    )
+
+    return x
