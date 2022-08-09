e0fac86f4dda93465baeda4dcee6a369425d7fde
Sangho Kim <shkimkitecs@gmail.com>
2022-08-09 10:44:02 +0900

    모델 예측 및 훈련 코드 수정

--- fds/model/inference.py
+++ fds/model/inference.py
@@ -5,7 +5,8 @@ from w2v import word2vec_load
 import os
 import tensorflow as tf
 from preprocessing import remove_spectial_character, tokenizer
-
+from attention import Attention
+from keras.models import load_model
 if __name__ == '__main__':
     # 현재 디렉토리 주소 /fds/crawler
     CUR_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -17,16 +18,20 @@ if __name__ == '__main__':
     print('------tokenized------')
     data['tokenized'] = data['text(clean)'].apply(tokenizer)
     x_data = data.tokenized
-    maxlen = 1582
+    maxlen = 1179
 
     print('------- w2v load ------')
-    tri_model = word2vec_load('{}/trained/w2v_model_1'.format(CUR_DIR))
+    tri_model = word2vec_load('{}/trained/w2v_model'.format(CUR_DIR))
     print('------ integer_indexing ------')
     x = integer_indexing(x_data, maxlen, tri_model, 'post')
     print('------model load---------')
-    lstm_model = build_lstm_model(tri_model.vectors, x.shape[1])
-    lstm_model.load_weights('{}/trained/lstm_model_1.h5'.format(CUR_DIR))
+    lstm_model = load_model('{}/trained/lstm_model.h5'.format(CUR_DIR), custom_objects={'Attention': Attention})
+    # lstm_model = build_lstm_model(tri_model.vectors, x.shape[1])
+    # lstm_model.load_weights('{}/trained/lstm_model_1.h5'.format(CUR_DIR))
     print('------ predict------')
+    print(lstm_model.layers[0].input_shape[1])
     pred = lstm_model.predict(x)
-    # pred = tf.argmax(pred)
+    print(pred)
+    print('argmax')
+    pred = tf.argmax(pred, axis=-1)
     print(pred)
--- fds/model/train.py
+++ fds/model/train.py
@@ -6,7 +6,8 @@ from w2v import word2vec_load
 import os
 import datas
 import pandas as pd
-from vectorize import vectorize_data, integer_indexing
+from vectorize import integer_indexing
+from preprocessing import tokenizer, remove_spectial_character
 
 
 def model_train(X, y, X_val, y_val, tri_model, batch_size, path):
@@ -22,11 +23,19 @@ if __name__ == '__main__':
     CUR_DIR = os.path.dirname(os.path.abspath(__file__))
     # 부모 디렉토리 주소 /fds/
     PARENT_DIR = os.path.dirname((CUR_DIR))
-
-    tri_model = word2vec_load('{}/trained/w2v_model'.format(CUR_DIR))
-    print('padding')
-    # 정수 패딩
+    print('------ data load ------')
     dataset = datas.load_data('{}/data/dataset/dataset.csv'.format(PARENT_DIR))
+    outlier = datas.get_outlier(dataset=dataset)
+    dataset = datas.remove_some_dataset(dataset, under=0, over=outlier['fishing'][1])
+
+    # preprocessing
+    print('------ preprocessing ------')
+    dataset = remove_spectial_character(dataset)
+    dataset['tokenized'] = dataset['text(clean)'].apply(tokenizer)
+    print('padding')
+
+    # 데이터셋 분리
+    print('------ dataset split ------')
     fishings = dataset[dataset['fishing'] == 1]
     normals = dataset[dataset['fishing'] == 0]
     train_fishings = fishings[:len(fishings)//3]
@@ -36,7 +45,6 @@ if __name__ == '__main__':
     val_normals = normals[len(normals)//3:2*len(normals)//3]
     test_normals = normals[2*len(normals)//3:]
 
-    print('split')
     train = pd.concat([train_fishings, train_normals], ignore_index=True)
     train = train.sample(frac=1).reset_index(drop=True)
 
@@ -46,11 +54,13 @@ if __name__ == '__main__':
     test = pd.concat([test_fishings, test_normals], ignore_index=True)
     test = test.sample(frac=1).reset_index(drop=True)
 
-    print('tokenized')
+    print('------ w2v load model ------')
+    tri_model = word2vec_load('{}/trained/w2v_model'.format(CUR_DIR))
+
+    print('------ integer indexing ------')
     X_data = train.tokenized
     input_length = max(list(map(len, X_data)))
 
-    print('integer indexing')
     X = integer_indexing(data=X_data, maxlen=input_length, tri_model=tri_model, padding='post')
     y = train['fishing'].values
 
@@ -60,8 +70,11 @@ if __name__ == '__main__':
 
     X_test = integer_indexing(data=test.tokenized, maxlen=input_length, tri_model=tri_model, padding='post')
 
-    print('build model')
+    print('------ build model ------')
     model = build_lstm_model(embedding_matrix=tri_model.vectors, input_length=X.shape[1])
     batch_size = 32
-    print('trained')
+    print('------ trained ------')
     trained = model_train(X, y, X_val, y_val, tri_model, batch_size=batch_size, path='{}/trained/lstm_model.h5'.format(CUR_DIR))
+    f = open("{}/trained/input_length".format(CUR_DIR), 'w+')
+    f.write(input_length)
+    f.close()
